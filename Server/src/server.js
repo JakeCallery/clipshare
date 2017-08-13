d0c700f8947ba7d227f89b9938987b303e303026
Jake Callery <jcallery@subvoicestudios.com>
2017-08-13 19:35:03 -0400

    clean up server side

--- Server/src/server.js
+++ Server/src/server.js
@@ -8,18 +8,14 @@ const app = express();
 
 let connections = [];
 
-
 app.use('/', express.static('../../dist'));
 
-/*
-app.listen(8888, '192.168.1.95', function () {
-    console.log('Example app listening on port 80!')
-});
-*/
 
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({
+    server:server
+});
 
 
 function originIsAllowed($origin) {
@@ -27,11 +23,33 @@ function originIsAllowed($origin) {
     return true;
 }
 
-wss.on('connection', ($ws, $req) => {
+wss.on('connection', ($connection, $req) => {
     const location = url.parse($req.url, true);
 
-    $ws.on('message', ($msg) => {
+    console.log((new Date()) + ' Connection accepted: ' + $req.connection.remoteAddress);
+    connections.push($connection);
+
+    $connection.on('message', ($msg) => {
+        let msgType = typeof $msg;
+        console.log('Type of message: ', msgType);
+
+
         console.log('Message: ', $msg);
+    });
+
+    $connection.on('close', ($code, $reason) => {
+        console.log((new Date()) + ' Peer ' + $connection.address + ' disconnected.');
+        console.log('ReasonCode: ', $code);
+        console.log('Desc: ', $reason);
+
+        //Remove connection
+        console.log('Removing Connection from pool');
+        for(let i = 0; i < connections.length; i++){
+            if(connections[i] === $connection){
+                console.log('Removing Connection: ', i);
+                connections.splice(i, 1);
+            }
+        }
     });
 
 });
@@ -39,50 +57,4 @@ wss.on('connection', ($ws, $req) => {
 server.listen(8888, () => {
     console.log('Listening on %d', server.address().port);
 });
-/*
-wsServer.on('request', ($req) => {
-    console.log('Request: ', $req);
-    //Validate Origin
-    if(!originIsAllowed($req.origin)){
-        $req.reject();
-        console.log((new Date()) + ' Connection from origin ' + $req.origin + ' rejected.');
-        return;
-    }
 
-    //Accept Connection
-    let connection = $req.accept('clip-proto', $req.origin);
-    console.log((new Date()) + ' Connection accepted.');
-
-    //Add connection to list
-    console.log('Adding Connection');
-    connections.push(connection);
-
-    //Listen for close and message
-    connection.on('message', ($message) => {
-        console.log('Caught Message of Type: ', $message.type);
-
-        if($message.type === 'utf8'){
-            //Text Message
-            console.log('Received utf8 Message: ' + $message.utf8Data);
-        } else if($message.type === 'binary') {
-            //Binary Data
-            console.log('Received Binary message of length(in bytes): ' + $message.binaryData.length);
-        }
-    });
-
-    connection.on('close', ($reasonCode, $desc) => {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-        console.log('ReasonCode: ', $reasonCode);
-        console.log('Desc: ', $desc);
-
-        //Remove connection
-        for(let i = 0; i < connections.length; i++){
-            if(connections[i] === connection){
-                console.log('Removing Connection: ', i);
-                connections.splice(i, 1);
-            }
-        }
-
-    });
-});
-*/
